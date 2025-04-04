10fd35b14b954b88a96cc59f42f5a2b7ba2864a3
Kyle McColgan <kdmbus21@gmail.com>
2025-04-04 16:49:22 -0500

    index.js: Replace outdated ReactDOM.render() method with newer ReactDOM.createRoot() syntax.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+//Date: 4 April 2025
+//File name: index.js
+//Author: Kyle McColgan
+//Description: Main entry point for the website.
+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Use 'react-dom/client' for React 18+...
 import App from './App'; // The main app component.
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root')); // Create root element...
+
+//Render the root element...
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />
+  </React.StrictMode>
 );
